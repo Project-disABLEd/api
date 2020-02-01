c42ea91eaafe328a0b7cbf560b78b4c635b19dec
Wuzado <wuzado@gmail.com>
2020-02-01 13:15:20 +0100

    Static files fix

--- api_project/settings.py
+++ api_project/settings.py
@@ -13,11 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from os import path
 from django.core.management.utils import get_random_secret_key
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -156,4 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
