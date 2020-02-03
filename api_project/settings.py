d15f97ccb1a05cd13863b5d0165fcb62455b95ea
Wuzado <wuzado@gmail.com>
2020-02-03 14:16:56 +0100

    Collectstatic fix
    I hope

--- api_project/settings.py
+++ api_project/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'assets'))
 STATICFILES_DIRS = (
     ('assets', 'static'),
 
