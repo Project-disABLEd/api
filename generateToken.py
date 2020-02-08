c5ef641e740cf4993267a96358795633cd33c7aa
jaanonim <mat8mro@gmail.com>
2020-02-08 15:58:08 +0100

    script to genarating token

---
+++ generateToken.py
@@ -0,0 +1,12 @@
+from jose import jwt
+import os
+
+SECRET_KEY_TOKEN_FILE = open("./api_project/secret_key_token.txt", "r")
+SECRET_KEY_TOKEN = SECRET_KEY_TOKEN_FILE.read()
+SECRET_KEY_TOKEN_FILE.close()
+
+
+token = jwt.encode({'key': 'value'}, SECRET_KEY_TOKEN, algorithm='HS256')
+print(token)
+
+os.system("pause")
