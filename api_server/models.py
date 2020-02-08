cb680ebe718b65122fa11b2f4eb0ba21518e0aac
jaanonim <mat8mro@gmail.com>
2020-02-08 17:07:12 +0100

    add validators for points

--- api_server/models.py
+++ api_server/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api_server.validators import validate_rating, validate_url, validate_latitude, validate_longitude
 
 class Point(models.Model):
     created = models.DateTimeField(auto_now_add=True) # Auto-created, returns date of creation
@@ -6,10 +7,11 @@ class Point(models.Model):
     name = models.CharField(max_length=100) # Name of the place, required
     source = models.TextField(max_length=100) # Source from which the data was taken (Manual, OpenStreetMap/WheelMap/accessibility.cloud, Town Hall), required
     desc = models.TextField(blank=True, default='') # Description of the place, optional
-    site = models.TextField(blank=True, default='') # WWW site of the place, optional
-    address = models.TextField() # Address of the place, required
-    latitude = models.FloatField(max_length=10, blank=True, default='0') # Latitude, optional (will be converted from the address ASAP by the curation team)
-    longitude = models.FloatField(max_length=10, blank=True, default='0') # Longitude, optional (will be converted from the address ASAP by the curation team)
+    site = models.TextField(blank=True, default='', validators=[validate_url]) # WWW site of the place, optional
+    address = models.TextField(blank=True, default='') # Address of the place, optional
+    latitude = models.FloatField(max_length=10, validators=[validate_latitude]) # Latitude, required
+    longitude = models.FloatField(max_length=10, validators=[validate_longitude]) # Longitude, required
+    typeObj = models.ForeignKey('TypeOfPoint', on_delete=models.CASCADE) # Type of point, required
     #opening_hours = models.DateTimeField()
 
     '''
@@ -21,31 +23,39 @@ class Point(models.Model):
     - 0 - Unknown support by the place.
     - -1 - The place does not support the thing in question.
     '''
+    maxLengthForDesc = 200 # Max length for all text fields below.
+    
+    wheelchair_accessibility = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    wheelchair_accessibility_desc = models.TextField(blank=True, default='')
 
-    wheelchair_accessibility = models.IntegerField(blank=True, default='0')
-    wheelchair_accessibility_desc = models.TextField(blank=True)
-    toilets = models.IntegerField(blank=True, default='0')
-    toilets_desc = models.TextField(blank=True, default='')
-    discounts = models.IntegerField(blank=True, default='0')
-    discounts_desc = models.TextField(blank=True, default='')
-    guide_dog = models.IntegerField(blank=True, default='0')
-    guide_dog_desc = models.TextField(blank=True, default='')
-    parking = models.IntegerField(blank=True, default='0')
-    parking_desc = models.TextField(blank=True, default='')
-    hearing_imp = models.IntegerField(blank=True, default='0')
-    hearing_imp_desc = models.TextField(blank=True, default='')
-    vision_imp = models.IntegerField(blank=True, default='0')
-    vision_imp_desc = models.TextField(blank=True, default='')
-    mental_disorder = models.IntegerField(blank=True, default='0')
-    mental_disorder_desc = models.TextField(blank=True, default='')
-    staff = models.IntegerField(blank=True, default='0')
-    staff_desc = models.TextField(blank=True, default='')
+    toilets = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    toilets_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
 
-    typeObj = models.ForeignKey('TypeOfPoint', on_delete=models.CASCADE)
+    discounts = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    discounts_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
+    guide_dog = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    guide_dog_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
+    parking = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    parking_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
+    hearing_imp = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    hearing_imp_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
+    vision_imp = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    vision_imp_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
+    mental_disorder = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    mental_disorder_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
+    staff = models.IntegerField(blank=True, default='0', validators=[validate_rating])
+    staff_desc = models.TextField(blank=True, default='', max_length=maxLengthForDesc)
+
 
     class Meta:
         ordering = ['ID']
         
 
 class TypeOfPoint(models.Model):
-    name=models.TextField()
+    name=models.TextField(max_length=100)
