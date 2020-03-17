3f1455dc637a0ce9ea2d451769a92959411c3fdd
jaanonim <mat8mro@gmail.com>
2020-03-17 12:10:03 +0100

    Simple test for Get and Post endpoints

--- api_server/tests.py
+++ api_server/tests.py
@@ -1,50 +1,92 @@
-from django.urls import include, path, reverse
 from rest_framework.test import APITestCase
-from api_project import urls
 from rest_framework import status
 
 
 class AccountTests(APITestCase):
 
-    def testPostPoints(self):
-        url = '/api/points/types/add/'
-        response = self.client.post(url, {'name': 'test'}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+    def testOneBigTest(self):
 
-    def testPostPointsType(self):
+        #-------Test Post-------
+        url = '/api/points/types/add/'
+        requreStatus = status.HTTP_201_CREATED
+        response = self.client.post(url, {'pk': 1, 'name': 'test'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        if(response.status_code == requreStatus):
+            print("P")
+        else:
+            print("Error")
+
+        #-------Test Post-------
         url = '/api/points/add/'
+        requreStatus = status.HTTP_201_CREATED
         response = self.client.post(url, {'name': 'test',
                                           'source': 'test',
-                                          'latitude': 0,
-                                          'longitude': 0,
+                                          'latitude': 1,
+                                          'longitude': 1,
                                           'typeObj': 1
                                           }, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
-
-    def testGetAllPoints(self):
+        if(response.status_code == requreStatus):
+            print("P")
+        else:
+            print("Error")
+   
+        #-------Test Get-------
         url = '/api/points/'
+        requreStatus = status.HTTP_200_OK
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        if(response.status_code == requreStatus):
+            print("G")
+        else:
+            print("Error")
 
-    def testGetPointsByPos(self):
+        #-------Test Get-------
         url = '/api/points/pos/'
+        requreStatus = status.HTTP_200_OK
         response = self.client.get(url, {'x': 1, 'y': 1}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK) 
-
-    def testGetPointsByRange(self):
-        url = '/api/points/range/'
-        response = self.client.get(url, {'x1': 1, 'y1': 1, 'x2': -1, 'y2': -1},format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
-
-    def testGetPointsByID(self):
-        url = '/api/points/1/'
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
-        
-    def testGetPointsTypeByID(self):
-        url = '/api/points/types/1/'
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)    
-
+        if(response.status_code == requreStatus):
+            print("G")
+        else:
+            print("Error")
     
+        #-------Test Get-------
+        url = '/api/points/search/'
+        requreStatus = status.HTTP_200_OK
+        response = self.client.get(url, {'phrase': "test"},format='json')
+        self.assertEqual(response.status_code, requreStatus)
+        if(response.status_code == requreStatus):
+            print("G")
+        else:
+            print("Error")
 
+        #-------Test Get-------
+        url = '/api/points/range/'
+        requreStatus = status.HTTP_200_OK
+        response = self.client.get(url, {'x1': 1, 'y1': 1, 'x2': -1, 'y2': -1},format='json')
+        self.assertEqual(response.status_code, requreStatus)
+        if(response.status_code == requreStatus):
+            print("G")
+        else:
+            print("Error")
+
+        #-------Test Get-------
+        url = '/api/points/1/'
+        requreStatus = status.HTTP_200_OK
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, requreStatus) 
+        if(response.status_code == requreStatus):
+            print("G")
+        else:
+            print("Error")
+
+        #-------Test Get-------
+        url = '/api/points/types/1/'
+        requreStatus = status.HTTP_200_OK
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, requreStatus)    
+        if(response.status_code == requreStatus):
+            print("G")
+        else:
+            print("Error")
--- api_server/urls.py
+++ api_server/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('points/pos/', getByPos),
     path('points/range/', getByRange),
     path('points/add/', postPoint),
+    path('points/search/', search),
     path('points/<int:pk>/', getByID),
     path('points/edit/<int:pk>/', editPoint),
     path('points/del/<int:pk>/', delPoint),
     path('points/types/<int:pk>/', getTypeByID),
     path('points/types/add/', postType),
-    path('points/search/', search),
     ]
