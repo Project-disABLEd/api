c9cd15bee9178fdf1bf01a7f94d1b4e83f321739
jaanonim <mat8mro@gmail.com>
2020-03-17 12:33:45 +0100

    Tests - done

--- api_server/tests.py
+++ api_server/tests.py
@@ -1,16 +1,26 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
-
+from jose import jwt
 
 class AccountTests(APITestCase):
 
     def testOneBigTest(self):
 
+        #-------Authorization-------
+        print("A")
+        SECRET_KEY_TOKEN_FILE = open("./api_project/secret_key_token.txt", "r")
+        SECRET_KEY_TOKEN = SECRET_KEY_TOKEN_FILE.read()
+        SECRET_KEY_TOKEN_FILE.close()
+        token = jwt.encode({'key': 'value'}, SECRET_KEY_TOKEN, algorithm='HS256')
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
         #-------Test Post-------
         url = '/api/points/types/add/'
         requreStatus = status.HTTP_201_CREATED
-        response = self.client.post(url, {'pk': 1, 'name': 'test'}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post(url, {'name': 'test'}, format='json')
+        
+        self.assertEqual(response.status_code, requreStatus)
         if(response.status_code == requreStatus):
             print("P")
         else:
@@ -25,7 +35,7 @@ class AccountTests(APITestCase):
                                           'longitude': 1,
                                           'typeObj': 1
                                           }, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+        self.assertEqual(response.status_code, requreStatus)    
         if(response.status_code == requreStatus):
             print("P")
         else:
@@ -35,7 +45,7 @@ class AccountTests(APITestCase):
         url = '/api/points/'
         requreStatus = status.HTTP_200_OK
         response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        self.assertEqual(response.status_code, requreStatus) 
         if(response.status_code == requreStatus):
             print("G")
         else:
@@ -45,7 +55,7 @@ class AccountTests(APITestCase):
         url = '/api/points/pos/'
         requreStatus = status.HTTP_200_OK
         response = self.client.get(url, {'x': 1, 'y': 1}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        self.assertEqual(response.status_code, requreStatus) 
         if(response.status_code == requreStatus):
             print("G")
         else:
@@ -88,5 +98,45 @@ class AccountTests(APITestCase):
         self.assertEqual(response.status_code, requreStatus)    
         if(response.status_code == requreStatus):
             print("G")
+        else:
+            print("Error")
+
+        #-------Test Patch-------
+        url = '/api/points/types/edit/1/'
+        requreStatus = status.HTTP_202_ACCEPTED
+        response = self.client.patch(url, {'name': 'test2'}, format='json')
+        self.assertEqual(response.status_code, requreStatus)    
+        if(response.status_code == requreStatus):
+            print("H")
+        else:
+            print("Error")
+
+        #-------Test Patch-------
+        url = '/api/points/edit/1/'
+        requreStatus = status.HTTP_202_ACCEPTED
+        response = self.client.patch(url, {'name': 'test2', 'latitude': 4}, format='json')
+        self.assertEqual(response.status_code, requreStatus)    
+        if(response.status_code == requreStatus):
+            print("H")
+        else:
+            print("Error")
+
+        #-------Test Delete-------
+        url = '/api/points/del/1/'
+        requreStatus = status.HTTP_204_NO_CONTENT
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, requreStatus)    
+        if(response.status_code == requreStatus):
+            print("D")
+        else:
+            print("Error")
+
+         #-------Test Delete-------
+        url = '/api/points/types/del/1/'
+        requreStatus = status.HTTP_204_NO_CONTENT
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, requreStatus)    
+        if(response.status_code == requreStatus):
+            print("D")
         else:
             print("Error")
--- api_server/urls.py
+++ api_server/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api_server.views import getByID, getByPos, getTypeByID, postPoint, postType, getByRange, getAll, search, editPoint, delPoint
+from api_server.views import getByID, getByPos, getTypeByID, postPoint, postType, getByRange, getAll, search, editPoint, delPoint, editType, delType
 
 urlpatterns = [
     path('points/', getAll),
@@ -12,4 +12,6 @@ urlpatterns = [
     path('points/del/<int:pk>/', delPoint),
     path('points/types/<int:pk>/', getTypeByID),
     path('points/types/add/', postType),
+    path('points/types/edit/<int:pk>/', editType),
+    path('points/types/del/<int:pk>/', delType),
     ]
